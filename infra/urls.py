8c6fe68eddf41e54a462294d715341b4f9bbedcc
yusuke8655 <mu103yu@gmail.com>
2024-02-21 00:10:13 +0900

    詳細ページ作成

--- infra/urls.py
+++ infra/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from infra.views import ComputerList
+from infra.views import ComputerList, ComputerDetail
 
 urlpatterns = [
     path("", ComputerList.as_view(), name="computer_list"),
+    path("computer/<int:pk>/", ComputerDetail.as_view(), name="computer_detail"),
 ]
--- infra/views.py
+++ infra/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from infra.models import Computer
 
 # Create your views here.
 class ComputerList(ListView):
     model = Computer
-    template_name = "infra/index.html"
     context_object_name = "computers" # templateに渡す変数名
+
+class ComputerDetail(DetailView):
+    model = Computer
+    context_object_name = "computer"
