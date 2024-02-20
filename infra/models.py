3bda472bc7df841887e25c7b8e96bca8cc14fe6f
yusuke8655 <mu103yu@gmail.com>
2024-02-20 23:21:36 +0900

    モデル(Computer)の追加

--- infra/models.py
+++ infra/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Computer(models.Model):
+    name = models.CharField('コンピューター名', max_length=20)
+    cpu = models.CharField('CPU', max_length=20)
+    memory_cap = models.IntegerField('メモリ容量', blank=True, null=True, default=0)
+    strage_cap = models.IntegerField('ストレージ容量', blank=True, null=True, default=0)
+    os = models.CharField('OS', max_length=20)
+    created_at = models.DateTimeField('登録日', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日', auto_now=True)
+
+    def __str__(self):
+        return self.name
