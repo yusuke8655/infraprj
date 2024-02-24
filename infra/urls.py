7b1901bccdec29830151c845674ac2a89f273fb2
yusuke8655 <mu103yu@gmail.com>
2024-02-24 23:05:37 +0900

    url指定方法変更

--- infra/urls.py
+++ infra/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from infra.views import ComputerList, ComputerDetail
 
+app_name = 'infra'
+
 urlpatterns = [
-    path("", ComputerList.as_view(), name="computer_list"),
-    path("computer/<int:pk>/", ComputerDetail.as_view(), name="computer_detail"),
+    path("", ComputerList.as_view(), name="list"),
+    path("computer/<int:pk>/", ComputerDetail.as_view(), name="detail"),
 ]
